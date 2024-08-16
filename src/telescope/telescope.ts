f7cc0b5593c5b5ef5d01ce2be502335eb68bd71a
g-nage <gn.nage@gmail.com>
2024-08-16 03:53:57 +0300

    change

--- src/telescope/telescope.ts
+++ src/telescope/telescope.ts
@@ -1,26 +1,33 @@
 // Lens but with Task integration ?
 
-const $first = Symbol();
-type $first = typeof $first;
+import { Pair } from "src/pair";
+import { Task } from "src/task";
 
-const $second = Symbol();
-type $second = typeof $second;
+const $get = Symbol();
+type $get = typeof $get;
 
-export interface Telescope<A, B> {
-	[$first]: A;
-	[$second]: B;
+const $set = Symbol();
+type $set = typeof $set;
+
+interface Error {
+	code: string;
+}
+
+export interface Telescope<S, V> {
+	[$get]: (source: S) => Task<Pair<Error, V>>;
+	[$set]: (source: S, value: V) => S;
 };
 
 export namespace Telescope {
-	export function create<A, B>(first: A, second: B): Telescope<A, B> {
-		return { [$first]: first, [$second]: second };
-	}
+	// export function create<S, V>(first: A, second: B): Telescope<S, V> {
+	// 	return { [$get]: first, [$set]: second };
+	// }
 
-	export function first<A, B>(pair: Telescope<A, B>) {
-		return pair[$first];
+	export function get<S, V>(pair: Telescope<S, V>) {
+		return pair[$get];
 	}
 	
-	export function second<A, B>(pair: Telescope<A, B>) {
-		return pair[$second];
+	export function set<S, V>(pair: Telescope<S, V>) {
+		return pair[$set];
 	}
 }
